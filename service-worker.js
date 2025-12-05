f408c2d43b4a230256d2c4be7b1251dc5485d3f2
ozkanceylann <ozkanceylann@gmail.com>
2025-12-05 19:35:05 +0300

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,26 +1,26 @@
-const CACHE = "siparis-cache-v1";
+const CACHE = "siparis-cache-v2";
 const ASSETS = [
-  "./",
-  "./index.html",
-  "./style.css",
-  "./app.js",
-  "./api.js",
-  "./manifest.json",
-  "./icon-192.png",
-  "./icon-512.png"
+  "/",
+  "/index.html",
+  "/style.css",
+  "/app.js",
+  "/api.js",
+  "/manifest.json",
+  "/icon-192.png",
+  "/icon-512.png"
 ];
 
 self.addEventListener("install", (e) => {
   e.waitUntil(
-    caches.open(CACHE).then((c) => c.addAll(ASSETS)).then(self.skipWaiting())
+    caches.open(CACHE).then((c) => c.addAll(ASSETS))
   );
 });
 
 self.addEventListener("activate", (e) => {
   e.waitUntil(
     caches.keys().then(keys =>
-      Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))
-    ).then(self.clients.claim())
+      Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
+    )
   );
 });
 
